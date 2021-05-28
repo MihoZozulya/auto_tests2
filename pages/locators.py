a0bed2b31042d20d5d8e91634b37b9124df49e35
Miho <zmyhail@gmail.com>
2021-05-28 10:29:29 +0300

    tested many testes

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,12 @@ class MainPageLocators():
 class LoginPageLocators():
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    NAME_BOOK_IN_ALERT = (By.CSS_SELECTOR, "div.alert:nth-child(1) > div:nth-child(2) > strong")
+    MESSAGE_TO_ADD_PRODUCT_TO_BASKET = (By.CSS_SELECTOR, "div.alert:nth-child(1) > div:nth-child(2)")
+    SUMM_IN_BASKET = (By.CSS_SELECTOR, "div.alert:nth-child(3) > div:nth-child(2) > p:nth-child(1) > strong")
+    MESSAGE_TO_SUMM_IN_BASKET = (By.CSS_SELECTOR, "div.alert:nth-child(3) > div:nth-child(2) > p:nth-child(1)")
+    NAME_BOOK = (By.CSS_SELECTOR, "h1")
+    PRICE_BOOK = (By.CSS_SELECTOR, "p.price_color")
---
+++ pages/product_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def go_to_basket(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket_button.click()
+
+    def should_be_correct_book(self):
+        assert self.browser.find_element(*ProductPageLocators.MESSAGE_TO_ADD_PRODUCT_TO_BASKET), "don't product in basket"
+        name_book_in_alert = self.browser.find_element(*ProductPageLocators.NAME_BOOK_IN_ALERT)
+        name_book = self.browser.find_element(*ProductPageLocators.NAME_BOOK)
+        assert name_book_in_alert.text == name_book.text, "don't correct product in basket"
+
+    def should_be_correct_price(self):
+        assert self.browser.find_element(*ProductPageLocators.MESSAGE_TO_SUMM_IN_BASKET), "don't message to price for product in basket"
+        summ_in_basket = self.browser.find_element(*ProductPageLocators.SUMM_IN_BASKET)
+        price_book = self.browser.find_element(*ProductPageLocators.PRICE_BOOK)
+        assert summ_in_basket.text == price_book.text, "don't correct price in basket"
